6627429dfec3ae96ff424d6316cc79c8efeb5620
fedutre <federicocesar@gmail.com>
2022-11-07 22:37:31 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import NavBar from './components/NavBar/NavBar';
+import './App.css';
+import ItemsListContainer from './components/ItemsListContainer/ItemsListContainer';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <ItemsListContainer name="Bienvenidos a mi tienda On Line"/>
+    </>
+  );
+}
+
+export default App;
